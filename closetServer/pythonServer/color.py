14b6af018e2d4244306e871e4bf171616b4d4709
조은결 <55574154+chohan3036@users.noreply.github.com>
2020-11-10 13:05:52 +0900

    Update color.py

--- closetServer/pythonServer/color.py
+++ closetServer/pythonServer/color.py
@@ -1,12 +1,39 @@
-from PIL import Image
+    # 이미지에 포함된 색상을 K=5 클러스터링 하는 코드
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.cluster import KMeans
 import webcolors
 
 def color(filestr):
-    img = Image.open(filestr)
+    # 이미지 불러와서 RGB로 색상 변환 및 FLATTEN
+    img = cv2.imread(filestr)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.reshape((img.shape[1] * img.shape[0], 3))
 
-    width, height = img.size
-    r, g, b = img.getpixel((width / 2, height / 2))
+    # K를 5로 설정하고 이미지에 fit
+    kmeans = KMeans(n_clusters=5)
+    s = kmeans.fit(img)
 
+    # 비율 계산을 위해 모든 픽셀의 라벨을 사용
+    labels = kmeans.labels_
+    labels = list(labels)
+
+    # 클러스터링 된 결과를 저장
+    centroid = kmeans.cluster_centers_
+
+    # 모든 라벨 중 현재 색상에 해당하는 라벨이 몇 개 인지 계산
+    percent = []
+    for i in range(len(centroid)):
+      j = labels.count(i)
+      j = j / (len(labels))
+      percent.append(j)
+
+    # 가장 비율이 높은 색상의 RGB 값을 반환
+    r, g, b = map(int, centroid[percent.index(max(percent))])
+
+    # 이후 webcolors 라이브러리 사용하여 RGB 값에 대한 색상이름을 문자열로 반환
     def closest_colour(requested_colour):
         min_colours = {}
         for key, name in webcolors.css3_hex_to_names.items():
